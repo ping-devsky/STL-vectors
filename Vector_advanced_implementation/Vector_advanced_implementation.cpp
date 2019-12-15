67fd0c06c2bccac0fdaeb770a9fdf9f10d045c2b
unknown <201912032@daiict.ac.in>
2019-12-15 21:22:17 +0530

    First commit is applied Vector_advanced

---
+++ Vector_advanced_implementation/Vector_advanced_implementation.cpp
@@ -0,0 +1,73 @@
+/*Name : ping-devsky
+Date  : 15-12-2019
+vector implementation with multiple vector within vector and many more containers within vector */
+#include<iostream>
+#include<vector>
+
+void vectorAddValues(std::vector<std::vector<int>>& abc)
+{
+
+	/*abc.push_back({ 1,2,3,4 });
+	abc.push_back({ 4,5,9,8,7 });
+	abc.push_back({ 4,9,8,7 });
+	abc.push_back({ 4,5,9,7 });
+	abc.push_back({ 5,9,8,7 });
+	abc.push_back({ 4,8,7 });*/
+
+	// Vector values can be added in this two ways : 
+
+	for (int i = 0; i < 10; i++)
+	{
+		std::vector<int> temp;
+		for (int j = 0; j < 5; j++)
+		{
+			temp.push_back(rand() % 100);
+
+		}
+		abc.push_back(temp);
+	}
+
+}
+
+void vectorDisplayValues(std::vector<std::vector<int>>& abc)
+{
+	for (auto it = abc.begin(); it != abc.end(); it++)
+	{
+		std::cout << std::endl << " {";
+		for (auto it2 = (*(it)).begin(); it2 != (*(it)).end(); it2++)
+		{
+			std::cout << *(it2) << " ";
+		}
+		std::cout << "}";
+
+	}	
+
+}
+
+int main()
+{
+	
+	std::vector<std::vector<int>> abc;
+ 
+	//assign vector within vector
+	vectorAddValues(abc);
+
+	//Retrieve vector within vector values
+	vectorDisplayValues(abc);
+
+	
+
+
+	
+
+
+
+
+
+
+
+	
+
+
+	return 0;
+}
